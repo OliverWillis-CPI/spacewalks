7b3025966675f6dca46689281273f7e9ef069eb3
OliverWillis-CPI <123963533+OliverWillis-CPI@users.noreply.github.com>
2026-03-10 12:11:12 +0000

    added additional crew size analysis

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import re
 
 
 def main(input_file, output_file, graph_file):
@@ -8,6 +9,9 @@ def main(input_file, output_file, graph_file):
 
     # Read the data from JSON file
     eva_data = read_json_to_dataframe(input_file)
+
+    # Add crew size variable to dataset
+    eva_data = add_crew_size_column(eva_data)
 
     # Convert and export data to CSV file
     write_dataframe_to_csv(eva_data, output_file)
@@ -94,9 +98,8 @@ def text_to_duration(duration):
         duration_hours (float): The duration in hours
     """
     hours, minutes = duration.split(":")
-    duration_hours = int(hours) + int(minutes)/6  # there is an intentional bug on this line (should divide by 60 not 6)
+    duration_hours = int(hours) + int(minutes)/60  # there is an intentional bug on this line (should divide by 60 not 6)
     return duration_hours
-
 
 def add_duration_hours(df):
     """
@@ -113,6 +116,56 @@ def add_duration_hours(df):
         text_to_duration
     )
     return df_copy
+
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
+
+def summary_duration_by_astronaut(df):
+    """
+    Summarise the duration data by each astronaut and saves resulting table to a CSV file
+
+    Args: 
+        df (pd.DataFrame): Input dataframe to be summarised
+
+    
+    Returns:
+        sum_by_astro (pd.DataFrame): Data frame with a row for each astronaut and a summarised column 
+    """
+    print(f'Calculating summary of total EVA time by astronaut')
+    subset = df.loc[:,['crew', 'duration']] # subset to work with only relevant columns
+    subset = add_duration_hours(subset) # need duration_hours for easier calcs
+    subset = subset.drop('duration', axis=1) # dropping the extra 'duration' column as it contains string values not suitable for calulations
+    subset = subset.groupby('crew').sum() 
+    return subset
 
 if __name__ == "__main__":
 
